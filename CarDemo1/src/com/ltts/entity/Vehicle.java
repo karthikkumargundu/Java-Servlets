4ece1cdfe445eb30cb97f0a2b0f7d45f3663d0f6
karthikkumargundu <gundukarthik.kumar@ltts.com>
2021-02-26 10:36:53 +0530

    first

---
+++ CarDemo1/src/com/ltts/entity/Vehicle.java
@@ -0,0 +1,5 @@
+package com.ltts.entity;
+
+public class Vehicle {
+
+}
---
+++ CarDemo1/src/com/ltts/service/VehicleService.java
@@ -0,0 +1,11 @@
+package com.ltts.service;
+
+import com.ltts.entity.Vehicle;
+
+public class VehicleService {
+	public static void main(String[] args) {
+		Vehicle c=new Vehicle();
+		
+	}
+
+}
